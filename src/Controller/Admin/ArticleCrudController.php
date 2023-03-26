f4543f43ed60099e73e20eff72e04fb68d9a7d5a
Romain Rouault <r.rouault@shopapplication.com>
2023-03-26 15:38:50 +0200

    Article CRUD: explicit fields with some basic conditions

--- src/Controller/Admin/ArticleCrudController.php
+++ src/Controller/Admin/ArticleCrudController.php
@@ -4,6 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Article;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ArticleCrudController extends AbstractCrudController
 {
@@ -12,14 +18,20 @@ class ArticleCrudController extends AbstractCrudController
         return Article::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')
+            ->onlyOnIndex();
+        yield TextField::new('name');
+        yield TextField::new('slug');
+        yield DateField::new('creationDate')
+            ->onlyWhenUpdating();
+        yield AssociationField::new('author');
+        yield TextEditorField::new('content')
+            ->onlyOnForms();
+        yield TextEditorField::new('excerpt');
+        yield BooleanField::new('published');
+        yield DateField::new('publishedDate')
+            ->onlyWhenUpdating();
     }
-    */
 }
